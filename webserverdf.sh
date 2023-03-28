031696bd8b245637ffe9b705bd48f484765dc241
Dor Feldman <feldman.dor@gmail.com>
2023-03-28 14:20:48 +0300

    updated webserver script

--- webserverdf.sh
+++ webserverdf.sh
@@ -6,10 +6,23 @@ echo $ACTION
 
 if [ "$ACTION" = "install" ]; then
     echo "Installing..."
+    git pull origin main
 elif [ "$ACTION" = "start" ]; then
     echo "Starting..."
+    terraform init
+    terraform apply -auto-approve
 elif [ "$ACTION" = "stop" ]; then
     echo "Stopping..."
+    terraform destroy -auto-approve
+elif [ "$ACTION" = "status" ]; then
+    echo "Status:"
+    res_list=$(terraform state list)
+    if [ -z "$res_list" ]; then
+        echo "Cluster is not running."
+    else
+        echo "Cluster is up and running."
+        echo Resources: $res_list
+    fi
 else
-    echo "Wrong action! Available actions: install, start, stop"
+    echo "Wrong action! Available actions: install, start, stop, status"
 fi
